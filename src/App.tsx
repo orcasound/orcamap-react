0a7f9114ae25a28779607c101a2221e73a817471
Gaurav Rajendra Sangle <gauravrsan@gmail.com>
2020-03-26 04:48:31 +0530

    Add zoom control buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Taskbar />
-        <Map //eslint-disable-next-line
-          style="mapbox://styles/mapbox/light-v9"
-          containerStyle={{
-            height: '100vh',
-            width: '100vw',
-          }}
-          zoom={[6.5]}
-          center={[-123.35, 48.41]}
-        />
+        <Map />
       </header>
     </div>
   )
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,10 +1,30 @@
-// import React from 'react';
+import React from 'react'
 import ReactMapboxGL from 'react-mapbox-gl'
+import { ZoomControl } from 'react-mapbox-gl'
 
-const Map = ReactMapboxGL({
+const ReactMap = ReactMapboxGL({
   // accessToken:
   accessToken:
     'pk.eyJ1Ijoic3RvcmNrcGhvdG9zIiwiYSI6ImNrODNvYmc0czAxazMzbW1yZDdyeWZoNjAifQ.PiAIkQrVS4a5c7dBgcElhQ',
 })
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div>
+        <ZoomControl />
+        <ReactMap
+          style="mapbox://styles/mapbox/light-v9"
+          containerStyle={{
+            height: '100vh',
+            width: '100vw',
+          }}
+          zoom={[6.5]}
+          center={[-123.35, 48.41]}
+        />
+      </div>
+    )
+  }
+}
 
 export default Map
