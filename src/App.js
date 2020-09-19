b9ebd3fa90caa6bec4b05fc1fd6f6c60fa38f4c8
James Pedersen <james00090@gmail.com>
2020-09-19 15:11:18 -0700

    got working icon on the map

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
+
+const Map = ReactMapboxGl( {
+  accessToken: "pk.eyJ1Ijoid2F0Y2hlcjAwMDkwIiwiYSI6ImNrZjFvanBqMzEyZXAycnBpZW4wcjZxZGEifQ.McDjwJKtdiwLb74QllMoeA"
+})
+
+const zoom = [8];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+    style="mapbox://styles/mapbox/streets-v8"
+    zoom={zoom}
+    containerStyle={{
+      height: "500px",
+      width: "500px"
+    }}>
+      <Layer
+        type="symbol"
+        id="marker"
+        layout={{ "icon-image": "marker-15" }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+      </Layer>
+    </Map>
   );
 }
 
