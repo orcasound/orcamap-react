e2956b8a812b629b8b204ea16918b8d006dddce9
James Pedersen <james00090@gmail.com>
2020-09-28 09:15:35 -0700

    multiple markers showing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Source} from 'react-mapbox-gl'
 
 import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
 
@@ -9,6 +10,48 @@ const Map = ReactMapboxGl( {
 })
 
 const zoom = [8];
+
+const hydrophonesGeoJSONSource = {
+  "type" : "geojson", 
+  "data" : {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -90.0715, 29.9510 ]
+        },
+        "properties": {
+          "name": "Fred",
+        "gender": "Male"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -92.7298, 30.7373 ]
+        },
+        "properties": {
+          "name": "Martha",
+        "gender": "Female"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -91.1473, 30.4711 ]
+        },
+        "properties": {
+          "name": "Zelda",
+        "gender": "Female"
+        }
+      }
+    ]
+  }
+}
 
 function App() {
   return (
@@ -19,13 +62,8 @@ function App() {
       height: "500px",
       width: "500px"
     }}>
-      <Layer
-        type="symbol"
-        id="marker"
-        layout={{ "icon-image": "marker-15" }}>
-        <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-        <Feature coordinates={[-0.481747846041145, 52.3233379650232]}/>
-      </Layer>
+      <Source id="mySourceID" geoJsonSource={hydrophonesGeoJSONSource} />
+      <Layer type="symbol" layout={{'icon-image' : 'harbor-15'}} sourceId="mySourceID" />
     </Map>
   );
 }
