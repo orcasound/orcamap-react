0fa35e740824a31d0553f6df2e93868f04e56455
Devesh <rockdev1234560@gmail.com>
2021-03-11 18:04:40 +0530

    files linted and new linting settings added for repo

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -20,33 +20,32 @@ const Marker: React.FC = () => {
   const [coordinates, setCoordinates] = useState(initialCoordinates)
   useEffect(function effectFunction() {
     async function loadSpreadsheet() {
-        try{
-          await doc.loadInfo()
-          const sheet = doc.sheetsByIndex[0]
-          console.log(sheet.title)
-          console.log(sheet.rowCount)
+      try {
+        await doc.loadInfo()
+        const sheet = doc.sheetsByIndex[0]
+        console.log(sheet.title)
+        console.log(sheet.rowCount)
 
-          // TODO: this currently returns a single row from a sheet with 2+ entries, so only one map point is returned from sheets.
-          const rows = await sheet.getRows()
-          console.log('rows', rows)
+        // TODO: this currently returns a single row from a sheet with 2+ entries, so only one map point is returned from sheets.
+        const rows = await sheet.getRows()
+        console.log('rows', rows)
 
-          console.log(rows[0].timestamp)
-          const coordinates = []
-          for (let i = 0; rows[i] != null && i < sheet.rowCount; i++) {
-            coordinates.push({ long: rows[i].longitude, lat: rows[i].latitude })
-          }
-          setCoordinates(coordinates)
+        console.log(rows[0].timestamp)
+        const coordinates = []
+        for (let i = 0; rows[i] != null && i < sheet.rowCount; i++) {
+          coordinates.push({ long: rows[i].longitude, lat: rows[i].latitude })
         }
-        catch(err){
-          console.log(err);
-        }
+        setCoordinates(coordinates)
+      } catch (err) {
+        console.log(err)
+      }
     }
     loadSpreadsheet()
   }, [])
   return (
     <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
       {coordinates &&
-        coordinates.map((row,index) => {
+        coordinates.map((row, index) => {
           return <Feature key={index} coordinates={[row.long, row.lat]} />
         })}
     </Layer>
