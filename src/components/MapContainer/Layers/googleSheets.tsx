a1b54cb683a73f2f3888e91cdd39b199d1de684b
Devesh <rockdev1234560@gmail.com>
2021-03-15 19:38:17 +0530

    added a vector layer for google sheets as well

---
+++ src/components/MapContainer/Layers/googleSheets.tsx
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect } from 'react'
+import MapContext from '../Map/MapContext'
+import OLVectorLayer from 'ol/layer/Vector'
+import { vector } from '../Source'
+import GeoJSON from 'ol/format/GeoJSON'
+import { get } from 'ol/proj'
+import hydropin from './hydropin.png'
+
+// note: both the VectorLayer styleOptions object
+// and the 'source' from line 33 will need to be hoisted
+//  to be able to make multiple different vector layers
+// for different data sources.
+
+import { Icon, Style } from 'ol/style'
+const styleOptions = {
+  MultiPointIcon: new Style({
+    image: new Icon({
+      src: hydropin,
+      scale: [0.25, 0.25],
+    }),
+  }),
+}
+
+interface props {
+  coordinates: number[][]
+  zIndex: number
+}
+
+const GoogleSheetsLayer: React.FC<props> = ({
+  coordinates,
+  zIndex = 0,
+}: props) => {
+  const { map } = useContext(MapContext)
+
+  useEffect(() => {
+    if (!map.addLayer) return
+
+    const vectorLayer = new OLVectorLayer({
+      source: vector({
+        features: new GeoJSON().readFeatures(
+          {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {
+                  kind: 'Sighting',
+                  name: 'SRKW',
+                  state: 'WA',
+                },
+                geometry: {
+                  type: 'MultiPoint',
+                  coordinates: coordinates,
+                },
+              },
+            ],
+          },
+          {
+            featureProjection: get('EPSG:3857'),
+          },
+        ),
+      }),
+      style: styleOptions.MultiPointIcon,
+    })
+
+    map.addLayer(vectorLayer)
+    vectorLayer.setZIndex(zIndex)
+
+    return () => {
+      if (map) {
+        map.removeLayer(vectorLayer)
+      }
+    }
+  }, [map, coordinates, zIndex])
+
+  return null
+}
+
+export default GoogleSheetsLayer
--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Map from './Map'
 import config from '../../config'
-import { Layers, TileLayer, VectorLayer } from './Layers'
+import { Layers, TileLayer, VectorLayer, GoogleSheetsLayer } from './Layers'
 import { fromLonLat } from 'ol/proj'
 import { Controls, FullScreenControl } from './Controls'
 import orca from './orcapin.png'
@@ -12,6 +12,7 @@ doc.useApiKey(config.apiKey)
 
 const MapContainer: React.FC = () => {
   const [coordinates, setCoordinates] = useState([[0, 0]])
+  const [googleSheetcoordinates, setgoogleSheetcoordinates] = useState([[0, 0]])
   const [zoom, setZoom] = useState(0)
   const [center, setCenter] = useState([0, 0])
   const [showLayer, setShowLayer] = useState(true)
@@ -24,13 +25,24 @@ const MapContainer: React.FC = () => {
         console.log(sheet.title)
         console.log(sheet.rowCount)
 
+        setCoordinates([
+          [-122.76045, 48.13569], // Port Townsend
+          [-122.6039, 48.03371], // Bush Point
+          [-123.17357, 48.55833], // Orcasound Lab
+          // [-122.4544, 47.3365],
+          // [-122.4768, 47.7365],
+          // [-122.4108, 47.7365],
+        ])
+        setZoom(9)
+        setCenter([-122.4713, 47.7237])
+
         // TODO: this currently returns a single row from a sheet with 2+ entries, so only one map point is returned from sheets.
         const rows = await sheet.getRows()
         console.log('rows', rows)
 
         console.log(rows[0].timestamp)
         for (let i = 0; rows[i] != null && i < sheet.rowCount; i++) {
-          setCoordinates((coordinates) => [
+          setgoogleSheetcoordinates((coordinates) => [
             ...coordinates,
             [rows[i].longitude, rows[i].latitude],
           ])
@@ -58,10 +70,44 @@ const MapContainer: React.FC = () => {
         Pod Sightings
       </div>
 
+      <h3
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Test map of test coordinates
+      </h3>
       <Map center={fromLonLat(center)} zoom={zoom}>
         <Layers>
           <TileLayer zIndex={0} />
           {showLayer && <VectorLayer coordinates={coordinates} zIndex={0} />}
+        </Layers>
+
+        <Controls>
+          <FullScreenControl />
+        </Controls>
+      </Map>
+
+      <h3
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Google Sheets Coordinates
+      </h3>
+      <Map center={fromLonLat(center)} zoom={zoom}>
+        <Layers>
+          <TileLayer zIndex={0} />
+          {showLayer && (
+            <GoogleSheetsLayer
+              coordinates={googleSheetcoordinates}
+              zIndex={0}
+            />
+          )}
         </Layers>
 
         <Controls>
