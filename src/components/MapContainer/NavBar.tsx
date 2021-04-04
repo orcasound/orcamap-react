c79600603560581d7a15f46437a97362c56d6c56
Devesh <rockdev1234560@gmail.com>
2021-04-04 20:37:42 +0530

    initialise the navbar styles and fix other styles

---
+++ src/components/MapContainer/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: '#333',
+        marginBottom: '10px',
+        paddingBottom: '10px',
+      }}
+      className="container"
+    >
+      <h1 style={{ display: 'flex', justifyContent: 'center', color: 'white' }}>
+        {' '}
+        (Test OL) Orca Map
+      </h1>
+    </div>
+  )
+}
+
+export default Nav
