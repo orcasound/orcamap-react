fe315cce97152c89be29d10801923fb579a50781
parth sarthi prasad <mepsp2000@gmail.com>
2021-04-09 18:35:22 +0530

    refactor(folder restructure and global styles import): refactor as per next requirements
    Folder refactor, global css files are placed in public directory, __app is created to import global
    styles places in styles directory, removing old react config, changing eslint to support next
    directory requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,5 +39,9 @@ module.exports = {
   rules: {
     '@typescript-eslint/dot-notation': 0,
     'no-plusplus': 0,
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
   },
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import dynamic from 'next/dynamic'
+import Layout from '../components/Layout'
+import './Map.module.css'
+
+const DisplayComponent = () => {
+  return <div> Loading ... </div>
+}
+
+const MapComponentWithNoSSR = dynamic(
+  () => import('../components/MapContainer/MapContainer'),
+  {
+    loading: () => DisplayComponent(),
+    ssr: false,
+  },
+)
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <MapComponentWithNoSSR />
+    </Layout>
+  )
+}
+
+export default App
